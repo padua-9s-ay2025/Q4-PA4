4ffb5ccb544439d0a82fa9c3e8e0d65ecf85834f
padua-9s-ay2025 <padua.aa@obmontessori.edu.ph>
2026-04-21 07:40:09 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function countingCars() {
+
+let carCounter = 18;
+const maxCars = 25;
+
+    while (carCounter < maxCars) {
+    window.alert("There are " + carCounter + " cars in the garage.");
+    carCounter += 1;
+}
+} 
